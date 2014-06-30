b06c16e9eb58b63ce10a58b91ce786aefaaf0264
Matthias Bethke <matthias@towiski.de>
2014-06-30 22:18:05 +0200

    Trying to deal with already started app

--- mysnmp.erl
+++ mysnmp.erl
@@ -3,8 +3,13 @@
 -export([main/1]).
 
 % 1.3.6.1.2.1.1.3.0
+query([]) ->
+    query([".1.3.6.1.2.1.1.3.0"]);
 query(Args) ->
-    ok = snmpm:async_get(snmp_user, "localhost", ".1.3.6.1.2.1.1.3.0").
+    case snmpm:async_get(snmp_user, "localhost", Args) of
+        {ok, ReqId} -> ReqId;
+        {error, Reason} -> throw(Reason)
+    end.
     
 main(Args) ->
 	Options = [
@@ -14,7 +19,11 @@ main(Args) ->
         {address, "localhost"},
         {timeout, 2000}
     ],
-    snmpm:start(),
+    try snmpm:start() of
+        _ -> ok
+        catch
+        error:Error -> ok
+    end,
     snmpm:register_user(snmp_user, snmp_user, undefined),
     snmpm:register_agent(snmp_user, "localhost", Options),
     query(string:tokens(Args," ")).
