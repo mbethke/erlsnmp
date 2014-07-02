d4966f5597cf9033e2e3429726599fd11a44dc0a
Matthias Bethke <matthias@towiski.de>
2014-07-03 00:38:28 +0200

    Basically works

--- mysnmp.erl
+++ mysnmp.erl
@@ -2,20 +2,17 @@
 
 -export([main/1]).
 
-% 1.3.6.1.2.1.1.3.0
-query([]) ->
-    query([".1.3.6.1.2.1.1.3.0"]);
 query(Args) ->
     case snmpm:async_get(snmp_user, "localhost", Args) of
         {ok, ReqId} -> ReqId;
-        {error, Reason} -> throw(Reason)
+        {error, Reason} -> throw(snmpm:format_reason(Reason))
     end.
     
 main(Args) ->
 	Options = [
         {engine_id, "engine"},
         {community, "public"},
-        {version, v2c},
+        {version, v2},
         {address, "localhost"},
         {timeout, 2000}
     ],
@@ -24,6 +21,15 @@ main(Args) ->
         catch
         error:Error -> ok
     end,
+    case snmpm:which_agents(snmp_user) of
+        [] -> ok;
+        _ -> snmpm:unregister_agent(snmp_user, "localhost")
+    end,
     snmpm:register_user(snmp_user, snmp_user, undefined),
-    snmpm:register_agent(snmp_user, "localhost", Options),
-    query(string:tokens(Args," ")).
+    ok = snmpm:register_agent(snmp_user, "localhost", Options),
+    %query([1,3,6,1,2,1,1,3,0]).
+    query(lists:map(
+            fun(Arg) -> [list_to_integer(N) || N <- string:tokens(Arg, ".")] end,
+            string:tokens(Args," ")
+        )
+    ).
